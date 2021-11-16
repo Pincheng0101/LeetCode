167729e71455ec879cb1252b2a32abab2cda1f92
Pincheng Shih <pincheng0101@gmail.com>
2021-11-16 21:08:22 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pincheng0101/leetcode
+
+go 1.16
---
+++ problems/0977.Squares-of-a-Sorted-Array/0977.go
@@ -0,0 +1,12 @@
+package problems
+
+import "sort"
+
+func sortedSquares(nums []int) []int {
+	for i := 0; i < len(nums); i++ {
+		nums[i] *= nums[i]
+	}
+	sort.Ints(nums)
+
+	return nums
+}
