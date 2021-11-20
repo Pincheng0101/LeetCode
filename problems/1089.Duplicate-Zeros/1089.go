ba90e0bdcfd7d37974b14e7b420e6015497fbd52
Pincheng Shih <pincheng0101@gmail.com>
2021-11-20 17:00:16 +0800

    Update 1089

--- problems/1089.Duplicate-Zeros/1089.go
+++ problems/1089.Duplicate-Zeros/1089.go
@@ -1,6 +1,7 @@
 package p1089
 
 // 1: 先複製一份 arr 出來再去修改原先的 arr
+// Runtime: 4ms, Memory Usage: 3.5MB
 func duplicateZeros_1(arr []int) {
 	arrCopy := make([]int, len(arr))
 	copy(arrCopy, arr)
@@ -16,5 +17,60 @@ func duplicateZeros_1(arr []int) {
 		}
 		arrIndex++
 		arrCopyIndex++
+	}
+}
+
+// 2: 先計算 0 的數量，再從後面往前補
+// Runtime: 4ms, Memory Usage: 3.3MB
+func duplicateZeros_2(arr []int) {
+	zeroCount := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			zeroCount++
+		}
+	}
+	for i := len(arr) - 1; i >= 0; i-- {
+		if arr[i] == 0 {
+			if i+zeroCount < len(arr) {
+				arr[i+zeroCount] = 0
+			}
+			if i+zeroCount-1 < len(arr) {
+				arr[i+zeroCount-1] = 0
+			}
+			zeroCount--
+		} else {
+			if i+zeroCount < len(arr) {
+				arr[i+zeroCount] = arr[i]
+			}
+		}
+	}
+}
+
+// 3: 同上，不過只計算複製 0 的數量，要額外判斷最後一個是 0 的情況，有可能不需要複製
+// Runtime: 4ms, Memory Usage: 3.3MB
+func duplicateZeros_3(arr []int) {
+	zeroDupCount := 0
+	arrLen := len(arr)
+	for i := 0; i < arrLen-zeroDupCount; i++ {
+		if arr[i] == 0 {
+			// 事先處理好最後一個是 0 的情況
+			// 後面只依靠 zeroDupCount 無法判斷最後一個是否需要複製 0
+			if i == arrLen-zeroDupCount-1 {
+				arrLen--
+				arr[arrLen] = 0
+				break
+			}
+			zeroDupCount++
+		}
+	}
+
+	for i := arrLen - zeroDupCount - 1; i >= 0; i-- {
+		if arr[i] == 0 {
+			arr[i+zeroDupCount] = 0
+			zeroDupCount--
+			arr[i+zeroDupCount] = 0
+		} else {
+			arr[i+zeroDupCount] = arr[i]
+		}
 	}
 }
--- problems/1089.Duplicate-Zeros/1089_test.go
+++ problems/1089.Duplicate-Zeros/1089_test.go
@@ -15,6 +15,9 @@ func testCases() []testCase {
 	return []testCase{
 		{[]int{1, 0, 2, 3, 0, 4, 5, 0}, []int{1, 0, 0, 2, 3, 0, 0, 4}},
 		{[]int{1, 2, 3}, []int{1, 2, 3}},
+		{[]int{0, 1, 7, 6, 0, 2, 0, 7}, []int{0, 0, 1, 7, 6, 0, 0, 2}},
+		{[]int{8, 4, 5, 0, 0, 0, 0, 7}, []int{8, 4, 5, 0, 0, 0, 0, 0}},
+		{[]int{1, 5, 2, 0, 6, 8, 0, 6, 0}, []int{1, 5, 2, 0, 0, 6, 8, 0, 0}},
 	}
 }
 
@@ -22,6 +25,22 @@ func TestDuplicateZeros_1(t *testing.T) {
 	testCases := testCases()
 	for _, testCase := range testCases {
 		duplicateZeros_1(testCase.Input)
+		assert.Equal(t, testCase.Output, testCase.Input)
+	}
+}
+
+func TestDuplicateZeros_2(t *testing.T) {
+	testCases := testCases()
+	for _, testCase := range testCases {
+		duplicateZeros_2(testCase.Input)
+		assert.Equal(t, testCase.Output, testCase.Input)
+	}
+}
+
+func TestDuplicateZeros_3(t *testing.T) {
+	testCases := testCases()
+	for _, testCase := range testCases {
+		duplicateZeros_3(testCase.Input)
 		assert.Equal(t, testCase.Output, testCase.Input)
 	}
 }
