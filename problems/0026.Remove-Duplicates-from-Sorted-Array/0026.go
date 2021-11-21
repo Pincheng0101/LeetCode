bcb61474792fd9ffe42307f3d2c33f724b77b632
Pincheng Shih <pincheng0101@gmail.com>
2021-11-21 22:48:23 +0800

    Add 0026

---
+++ problems/0026.Remove-Duplicates-from-Sorted-Array/0026.go
@@ -0,0 +1,19 @@
+package p0026
+
+// 題目有提到不用管後面的值是什麼，只要把不重複的數字移到前面就行
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	n := 1
+	v := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != v {
+			nums[n] = nums[i]
+			v = nums[i]
+			n++
+		}
+	}
+
+	return n
+}
---
+++ problems/0026.Remove-Duplicates-from-Sorted-Array/0026_test.go
@@ -0,0 +1,33 @@
+package p0026
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	Input  []int
+	Output int
+}
+
+func testCases() []testCase {
+	return []testCase{
+		{
+			Input:  []int{1, 1, 2},
+			Output: 2,
+		},
+		{
+			Input:  []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			Output: 5,
+		},
+	}
+}
+
+func TestRemoveElement(t *testing.T) {
+	testCases := testCases()
+	for _, testCase := range testCases {
+		output := removeDuplicates(testCase.Input)
+		assert.Equal(t, testCase.Output, output)
+	}
+}
