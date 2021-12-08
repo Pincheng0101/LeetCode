b6e835e50f2693a44a7d2c552d0b1394e71375e3
Pincheng Shih <pincheng0101@gmail.com>
2021-12-09 03:01:43 +0800

    Add 0563 simple recursive code

---
+++ problems/0563.Binary-Tree-Tilt/0563.go
@@ -0,0 +1,30 @@
+package p0563
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findTilt(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return abs(sumNode(root.Left)-sumNode(root.Right)) + findTilt(root.Left) + findTilt(root.Right)
+}
+
+func sumNode(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+	return node.Val + sumNode(node.Left) + sumNode(node.Right)
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
