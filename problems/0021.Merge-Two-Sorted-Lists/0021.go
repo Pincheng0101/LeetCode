be45d9ed901e4153ae90be9d944cac8e0b1b907f
Pincheng Shih <pincheng0101@gmail.com>
2021-12-31 23:51:15 +0800

    Add 0021

---
+++ problems/0021.Merge-Two-Sorted-Lists/0021.go
@@ -0,0 +1,64 @@
+package p0021
+
+import . "github.com/pincheng0101/leetcode/datastructures/linkedlist"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists1(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	head := (*ListNode)(nil)
+	if list1.Val < list2.Val {
+		head = list1
+		list1 = list1.Next
+	} else {
+		head = list2
+		list2 = list2.Next
+	}
+
+	cur := head
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+
+	if list1 != nil {
+		cur.Next = list1
+	}
+	if list2 != nil {
+		cur.Next = list2
+	}
+	return head
+}
+
+// 遞迴
+func mergeTwoLists2(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists2(list1.Next, list2)
+		return list1
+	}
+	list2.Next = mergeTwoLists2(list1, list2.Next)
+	return list2
+}
---
+++ problems/0021.Merge-Two-Sorted-Lists/0021_test.go
@@ -0,0 +1,60 @@
+package p0021
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/pincheng0101/leetcode/datastructures/linkedlist"
+)
+
+type Input struct {
+	list1 []int
+	list2 []int
+}
+type testCase struct {
+	Input  Input
+	Output []int
+}
+
+func testCases() []testCase {
+	return []testCase{
+		{Input{[]int{1, 2, 4}, []int{1, 3, 4}}, []int{1, 1, 2, 3, 4, 4}},
+		{Input{[]int{}, []int{}}, []int{}},
+		{Input{[]int{}, []int{0}}, []int{0}},
+	}
+}
+
+func TestMergeTwoLists1(t *testing.T) {
+	testCases := testCases()
+	for _, testCase := range testCases {
+		list1 := NewSinglyLinkedList()
+		for i := 0; i < len(testCase.Input.list1); i++ {
+			list1.AddAtTail(testCase.Input.list1[i])
+		}
+		list2 := NewSinglyLinkedList()
+		for i := 0; i < len(testCase.Input.list2); i++ {
+			list2.AddAtTail(testCase.Input.list2[i])
+		}
+		output := mergeTwoLists1(list1.Head.Next, list2.Head.Next)
+		outputTestData := output.Slice()
+		assert.Equal(t, testCase.Output, outputTestData)
+	}
+}
+
+func TestMergeTwoLists2(t *testing.T) {
+	testCases := testCases()
+	for _, testCase := range testCases {
+		list1 := NewSinglyLinkedList()
+		for i := 0; i < len(testCase.Input.list1); i++ {
+			list1.AddAtTail(testCase.Input.list1[i])
+		}
+		list2 := NewSinglyLinkedList()
+		for i := 0; i < len(testCase.Input.list2); i++ {
+			list2.AddAtTail(testCase.Input.list2[i])
+		}
+		output := mergeTwoLists2(list1.Head.Next, list2.Head.Next)
+		outputTestData := output.Slice()
+		assert.Equal(t, testCase.Output, outputTestData)
+	}
+}
