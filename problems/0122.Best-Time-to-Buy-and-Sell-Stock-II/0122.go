c023e17ba09f5f6298da67bd4fa6fd249d267084
Pincheng Shih <pincheng0101@gmail.com>
2022-01-10 22:14:31 +0800

    Add 0122

---
+++ problems/0122.Best-Time-to-Buy-and-Sell-Stock-II/0122.go
@@ -0,0 +1,13 @@
+package p0122
+
+func maxProfit(prices []int) int {
+	var maxProfit int
+
+	for i := 1; i < len(prices); i++ {
+		if profit := prices[i] - prices[i-1]; profit > 0 {
+			maxProfit += profit
+		}
+	}
+
+	return maxProfit
+}
