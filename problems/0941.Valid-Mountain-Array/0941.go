554db748aa0004a07866817af05decb62ceb5f1f
Pincheng Shih <pincheng0101@gmail.com>
2022-01-25 01:04:34 +0800

    Optimze 0941

--- problems/0941.Valid-Mountain-Array/0941.go
+++ problems/0941.Valid-Mountain-Array/0941.go
@@ -1,27 +1,25 @@
 package p0941
 
-// 先遞增，後遞減
 func validMountainArray(arr []int) bool {
 	if len(arr) < 3 {
 		return false
 	}
-	up := arr[1] > arr[0]
-	if !up {
+
+	n := len(arr)
+	i := 0
+	// walk up
+	for i < n-1 && arr[i] < arr[i+1] {
+		i++
+	}
+
+	if i == 0 || i == n-1 {
 		return false
 	}
-	for i := 2; i < len(arr); i++ {
-		if arr[i] == arr[i-1] {
-			return false
-		}
-		if up {
-			if arr[i] < arr[i-1] {
-				up = false
-			}
-		} else {
-			if arr[i] > arr[i-1] {
-				return false
-			}
-		}
+
+	// walk down
+	for i < n-1 && arr[i] > arr[i+1] {
+		i++
 	}
-	return !up
+
+	return i == n-1
 }
