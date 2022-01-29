d72da043da822fac71a00518a49b480cdab9366c
Pincheng Shih <pincheng0101@gmail.com>
2022-01-29 16:50:43 +0800

    Optimze 0605

--- problems/0605.Can-Place-Flowers/0605.go
+++ problems/0605.Can-Place-Flowers/0605.go
@@ -1,20 +1,14 @@
 package p0605
 
 func canPlaceFlowers(flowerbed []int, n int) bool {
-	for i := 0; i < len(flowerbed) && n >= 0; {
-		if flowerbed[i] == 1 {
-			i += 2
-			continue
-		}
-		if i+1 == len(flowerbed) {
+	size := len(flowerbed)
+	for i := 0; i < size && n >= 0; i++ {
+		if flowerbed[i] == 0 &&
+			(i == 0 || flowerbed[i-1] == 0) &&
+			(i == size-1 || flowerbed[i+1] == 0) {
 			n--
-		} else if flowerbed[i+1] == 0 {
-			n--
-			i += 2
-			continue
+			i++
 		}
-		i++
 	}
-
 	return n <= 0
 }
