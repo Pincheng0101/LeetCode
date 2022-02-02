3e3797b52a45f86aa04b1d3519f0a3a2479f82ca
Pincheng Shih <pincheng0101@gmail.com>
2022-02-03 01:12:31 +0800

    Update 0438

--- problems/0438.Find-All-Anagrams-in-a-String/0438.go
+++ problems/0438.Find-All-Anagrams-in-a-String/0438.go
@@ -2,13 +2,14 @@ package p0438
 
 // Sliding Window
 func findAnagrams(s string, p string) []int {
-	chars, flag := [26]int{}, [26]bool{}
+	flag, chars := [26]bool{}, [26]int{}
 	for i := 0; i < len(p); i++ {
 		flag[val(p[i])] = true
 		chars[val(p[i])]++
 	}
-	cnt, left, ans := 0, 0, []int{}
-	for right := 0; right < len(s); {
+	left, right, cnt, ans := 0, 0, 0, []int{}
+	for right < len(s) {
+		// check right and resources are available
 		if flag[val(s[right])] && chars[val(s[right])] > 0 {
 			if cnt == 0 {
 				left = right
@@ -17,11 +18,13 @@ func findAnagrams(s string, p string) []int {
 			cnt++
 			if cnt == len(p) && right-left+1 == len(p) {
 				ans = append(ans, left)
+				// left recycle
 				chars[val(s[left])]++
 				left++
 				cnt--
 			}
 			right++
+			// check left and recycle, then let right check again (do not right++)
 		} else if flag[val(s[left])] {
 			chars[val(s[left])]++
 			left++
