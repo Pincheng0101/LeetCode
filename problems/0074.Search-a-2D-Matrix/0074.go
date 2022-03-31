defbbe0fa29c082f69a8ddb8a055c6457b15326f
Pincheng Shih <pincheng0101@gmail.com>
2022-04-01 00:44:18 +0800

    Optimize 0074

--- problems/0074.Search-a-2D-Matrix/0074.go
+++ problems/0074.Search-a-2D-Matrix/0074.go
@@ -1,33 +1,19 @@
 package p0074
 
 func searchMatrix(matrix [][]int, target int) bool {
-	top, bottom := 0, len(matrix)-1
-	for top <= bottom {
-		mid := top + (bottom-top)/2
-		if matrix[mid][0] == target {
-			return true
-		}
-		if matrix[mid][0] < target {
-			top = mid + 1
-		} else {
-			bottom = mid - 1
-		}
-	}
-	if top > 0 {
-		top--
-	}
-	left, right := 0, len(matrix[top])-1
-	for left <= right {
-		mid := left + (right-left)/2
-		if matrix[top][mid] == target {
-			return true
-		}
-		if matrix[top][mid] < target {
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-
-	return false
+    row, col := len(matrix), len(matrix[0])
+    left, right := 0, row*col-1
+    for left <= right {
+        mid := left + (right - left)/2
+        v := matrix[mid/col][mid%col]
+        if v == target {
+            return true
+        }
+        if v < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return false
 }
