2fbde0f1c593051e4c64ccfdcdb64789d8f92b75
Pincheng Shih <pincheng0101@gmail.com>
2022-04-04 21:20:09 +0800

    Optimize 1721

--- problems/1721.Swapping-Nodes-in-a-Linked-List/1721.go
+++ problems/1721.Swapping-Nodes-in-a-Linked-List/1721.go
@@ -11,11 +11,12 @@ import . "github.com/pincheng0101/leetcode/datastructures/linkedlist"
  */
 func swapNodes(head *ListNode, k int) *ListNode {
 	node1, node2, node2fast := head, head, head
+	// find node1 position
 	for i := 1; i < k; i++ {
 		node1 = node1.Next
-		node2fast = node2fast.Next
 	}
-	node2fast = node2fast.Next
+	// take k steps ahead of node2
+	node2fast = node1.Next
 
 	for node2fast != nil {
 		node2 = node2.Next
