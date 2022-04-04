9ea66cd9902b1c3863fc57d0fff5748568065bdb
Pincheng Shih <pincheng0101@gmail.com>
2022-04-04 21:43:14 +0800

    Rename fibCache func to fibWithCache

--- problems/0509.Fibonacci-Number/0509.go
+++ problems/0509.Fibonacci-Number/0509.go
@@ -9,8 +9,8 @@ func fib_1(n int) int {
 
 func fib_2(n int) int {
 	cache := map[int]int{}
-	var fibCache func(int) int
-	fibCache = func(nn int) int {
+	var fibWichCache func(int) int
+	fibWichCache = func(nn int) int {
 		if nn <= 1 {
 			return nn
 		}
@@ -18,18 +18,18 @@ func fib_2(n int) int {
 		if cache[nn-1] > 0 {
 			sum += cache[nn-1]
 		} else {
-			v := fibCache(nn - 1)
+			v := fibWichCache(nn - 1)
 			cache[nn-1] = v
 			sum += v
 		}
 		if cache[nn-2] > 0 {
 			sum += cache[nn-2]
 		} else {
-			v := fibCache(nn - 2)
+			v := fibWichCache(nn - 2)
 			cache[nn-2] = v
 			sum += v
 		}
 		return sum
 	}
-	return fibCache(n)
+	return fibWichCache(n)
 }
