26acd3433182b1ad2e057e38f8a76724ce7a2c66
Pincheng Shih <pincheng0101@gmail.com>
2022-08-28 22:14:17 +0800

    Add 1162 bfs

--- problems/1162.As-Far-from-Land-as-Possible/1162.go
+++ problems/1162.As-Far-from-Land-as-Possible/1162.go
@@ -1,7 +1,9 @@
 package p1162
 
+import "container/list"
+
 // dfs
-func maxDistance(grid [][]int) int {
+func maxDistance_dfs(grid [][]int) int {
 	for row := 0; row < len(grid); row++ {
 		for column := 0; column < len(grid[0]); column++ {
 			if grid[row][column] == 1 {
@@ -40,4 +42,45 @@ func max(a int, b int) int {
 		return a
 	}
 	return b
+}
+
+// bfs
+func maxDistance(grid [][]int) int {
+	queue := list.New()
+
+	for row := 0; row < len(grid); row++ {
+		for column := 0; column < len(grid[0]); column++ {
+			if grid[row][column] == 1 {
+				queue.PushBack([]int{row + 1, column})
+				queue.PushBack([]int{row - 1, column})
+				queue.PushBack([]int{row, column + 1})
+				queue.PushBack([]int{row, column - 1})
+			}
+		}
+	}
+
+	step := 0
+	for queue.Len() != 0 {
+		step++
+		qLen := queue.Len()
+		for i := 0; i < qLen; i++ {
+			front := queue.Front()
+			d := front.Value.([]int)
+			row, column := d[0], d[1]
+			queue.Remove(front)
+			if row >= 0 && column >= 0 && row < len(grid) && column < len(grid[0]) && grid[row][column] == 0 {
+				grid[row][column] = step
+				queue.PushBack([]int{row + 1, column})
+				queue.PushBack([]int{row - 1, column})
+				queue.PushBack([]int{row, column + 1})
+				queue.PushBack([]int{row, column - 1})
+			}
+		}
+	}
+
+	if step <= 1 {
+		return -1
+	}
+
+	return step - 1
 }
